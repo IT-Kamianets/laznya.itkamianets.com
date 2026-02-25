c37deb35ca840b8b5831a70cc08a9c8277584122
AnatoliyKN1B22 <kn1b22.humelnyk@kpnu.edu.ua>
2026-02-25 13:55:32 +0200

    Add ngx-translate for multilingual support and update translations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader, provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    
+    provideHttpClient(),
+    provideTranslateHttpLoader({
+      prefix: './assets/i18n/',
+      suffix: '.json'
+    }),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useClass: TranslateHttpLoader
+        },
+        defaultLanguage: 'ua'
+      })
+    )
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,40 @@
-import { Component, signal, OnInit } from '@angular/core';
+import { Component, signal, OnInit, inject } from '@angular/core';
 import AOS from 'aos';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
+  private translate = inject(TranslateService);
+  
   protected readonly title = signal('laznya-app');
   protected isMenuOpen = signal(false);
+  protected currentLang = signal('ua');
 
   toggleMenu() {
     this.isMenuOpen.update(val => !val);
   }
 
+  changeLang(lang: string) {
+    this.translate.use(lang);
+    this.currentLang.set(lang);
+  }
+
   ngOnInit() {
+    this.translate.addLangs(['ua', 'en', 'de', 'it', 'fr']);
+    this.translate.setDefaultLang('ua');
+    
+    // Check if there's a saved language or use browser language
+    const browserLang = this.translate.getBrowserLang();
+    const targetLang = browserLang && ['ua', 'en', 'de', 'it', 'fr'].includes(browserLang) ? browserLang : 'ua';
+    
+    this.translate.use(targetLang);
+    this.currentLang.set(targetLang);
+
     AOS.init({
       duration: 800,
       easing: 'ease-in-out',
